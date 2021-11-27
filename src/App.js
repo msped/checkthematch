dc6a64bb5681ffae5b93c2b9e80cff2212194872
Matt Edwards <mattsp.edwards@googlemail.com>
2021-11-27 20:56:56 +0000

    added themeprovider for darkmode and header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+import Header from './components/Header'
+
+let theme = createTheme({
+  palette: {
+    mode: "dark",
+  }
+})
+
+theme = responsiveFontSizes(theme)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Box mt={3}>
+          <Container maxWidth="md">
+            
+          </Container>
+        </Box>
+      </ThemeProvider>
     </div>
   );
 }
