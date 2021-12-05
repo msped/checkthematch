0ea64ea9c183b30a62ab9e217ec93fac780704c6
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-05 19:49:40 +0000

    added routes, header, styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
+import {
+  Route,
+  Routes 
+} from 'react-router-dom'
 
 import Header from './components/Header'
+import Home from './components/Home'
+import League from './components/League'
 
 let theme = createTheme({
   palette: {
@@ -21,7 +27,10 @@ function App() {
         <Header />
         <Box mt={3}>
           <Container maxWidth="md">
-            
+            <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route path="/league/:league" element={<League/>}/>
+            </Routes>
           </Container>
         </Box>
       </ThemeProvider>
