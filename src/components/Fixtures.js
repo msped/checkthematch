5349bb9132c0efafd46c3f59f4fb7b50b7568c88
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-23 14:55:57 +0000

    refactor: changed api call to only get full time matches

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -11,7 +11,7 @@ export default function Fixtures({leagueID, season}) {
     useEffect(() => {
         const search = async () => {
             const { data }  = await axios.get('https://api-football-v1.p.rapidapi.com/v3/fixtures', {
-              params: { league: leagueID, season: season, last: lastAmount.toString() },
+              params: { league: leagueID, season: season, last: lastAmount.toString(), status: 'FT' },
               headers: {
                 'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
                 'x-rapidapi-key': 'f89acc49f0mshfc233a01bb1f12dp1cdc9cjsndf01fbd6276c'
