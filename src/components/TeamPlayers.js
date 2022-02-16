01e1406bd6ef1ed98d92a6cc3b8eeca1bf6ed3ba
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-16 22:17:34 +0000

    feat: added component for team players

---
+++ src/components/TeamPlayers.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Grid, Typography, Stack, Box } from '@mui/material'
+
+import Player from '../components/Player'
+
+export default function TeamPlayers({ lineup }) {
+  return (
+    <Box sx={{ mx: 4 }}>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Stack alignItems="center">
+                    <Typography variant='h4'>{lineup.formation}</Typography>
+                </Stack>
+            </Grid>
+            {/* Starting XI */}
+            <Grid item xs={12} md={6}>
+                <Stack direction="column" spacing={2}>
+                    <Stack alignItems="center">
+                        <Typography variant='h5'>Starting XI</Typography>
+                    </Stack>
+                    {lineup.startXI.map((item) => (
+                        <Player key={item.player.id} player={item.player} />
+                    ))}
+                </Stack>
+            </Grid>
+
+            {/* Subs */}
+            <Grid item xs={12} md={6}>
+                <Stack direction="column" spacing={2}>
+                    <Stack alignItems="center">
+                        <Typography variant='h5'>Substitutes</Typography>
+                    </Stack>
+                    {lineup.substitutes.map((item) => (
+                        <Player key={item.player.id} player={item.player} />
+                    ))}
+                </Stack>
+            </Grid>
+        </Grid>
+    </Box>
+  )
+}
