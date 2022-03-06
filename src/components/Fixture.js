819e6f018921769c4265425506f85f5a3f5d3e4a
Matt Edwards <mattsp.edwards@googlemail.com>
2022-03-06 15:51:26 +0000

    refactor: removed unused box and stack imports

--- src/components/Fixture.js
+++ src/components/Fixture.js
@@ -4,8 +4,6 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack'
-import Box from '@mui/material/Box'
 
 import Statistics from '../components/Statistics';
 
