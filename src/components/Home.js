00902b1285dc36455ac0c87c8b5c5e422412e5c8
Matt Edwards <mattsp.edwards@googlemail.com>
2022-03-11 17:02:08 +0000

    refactor: optimized font loading on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,52 @@ import React from 'react'
 import { Grid, Typography, Container } from '@mui/material'
 
 import TopLeaguesGrid from '../components/TopLeaguesGrid'
+import home_banner from '../images/home_banner.jpg'
 
 export default function Home() {
 
     return (
     <Grid container spacing={1}>
         <Grid item xs={12}>
-            <div className='image-container'>
-                <div className='image-text'>
+            <div 
+                style={{
+                    backgroundImage: `url(${home_banner})`,
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'bottom',
+                    minHeight: '60vh'
+                }}
+            >
+                <div 
+                    style={{
+                        position: 'absolute',
+                        top: '30%',
+                        left: '7.5%'
+                    }}
+                >
                     <Typography
                         sx={{ typography: { xs:'h3', md: 'h3'}, marginBottom: '1%' }}
+                        style={{
+                            fontWeight: 500,
+                            fontStyle: 'italic',
+                            backgroundColor: '#000',
+                            width: 'fit-content',
+                            padding: '5px 15px',
+                            fontFamily: 'Patua One, cursive'
+                        }}
                      >
                         Check the Match
                     </Typography>
                     <Typography 
                         sx={{ typography: { xs:'body1', md: 'body1'} }}
+                        style={{
+                            fontWeight: 500,
+                            fontStyle: 'italic',
+                            backgroundColor: '#000',
+                            width: 'fit-content',
+                            padding: '5px 15px',
+                            fontFamily: 'Patua One, cursive'
+                        }}
                     >
                         See the stats before spending the time watching a potential boring game.
                     </Typography>
