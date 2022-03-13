e3feb5ba158a52a3592d79f33eb976a9f4290e47
Matt Edwards <mattsp.edwards@googlemail.com>
2022-03-13 02:22:29 +0000

    style: removed unused box import

--- src/components/Fixture.js
+++ src/components/Fixture.js
@@ -3,7 +3,6 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
 
 import Statistics from '../components/Statistics';
 
