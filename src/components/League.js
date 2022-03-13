3621c5ac3e28bd44ced09d9a40409707ebc72bdd
Matt Edwards <mattsp.edwards@googlemail.com>
2022-03-13 13:23:22 +0000

    refactor: moved fixture skeleton

--- src/components/League.js
+++ src/components/League.js
@@ -15,7 +15,6 @@ import Stack from '@mui/material/Stack';
 
 import Fixtures from '../components/Fixtures'
 import LeagueSkeleton from '../components/LeagueSkeleton'
-import FixturesSkeleton from '../components/FixturesSkeleton'
 
 export default function League() {
     const currentYear = new Date().getFullYear()
@@ -119,16 +118,7 @@ export default function League() {
                     { loading ? <LeagueSkeleton /> : leagueInfo()}
                 </Grid>
                 <Grid item xs={12}>
-                    { loading ? 
-                        <Stack spacing={2}>
-                            <FixturesSkeleton />
-                            <FixturesSkeleton />
-                            <FixturesSkeleton />
-                            <FixturesSkeleton />
-                        </Stack>
-                        : 
-                        <Fixtures leagueID={league_id} season={season}/>
-                    }
+                    <Fixtures leagueID={league_id} season={season}/>
                 </Grid>
             </Grid>
         </Container>
