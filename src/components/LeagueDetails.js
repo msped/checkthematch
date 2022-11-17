d41533c5a392bf7b24c9a1bfe189e48fc73ebee6
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-17 22:27:00 +0000

    feat: moved LeagueDetails into own component

---
+++ src/components/LeagueDetails.js
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+    Card,
+    CardContent,
+    Grid,
+    Typography,
+    Stack,
+    TextField,
+} from "@mui/material";
+
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+
+export default function leagueDetails({ league, season, setSeason }) {
+    return (
+        <Card>
+            <CardContent>
+                <Grid container spacing={2}>
+                    <Grid item xs={4} md={2}>
+                        <img
+                            src={league.league.logo}
+                            alt={league.league.name}
+                            width="150"
+                            height="150"
+                            style={{
+                                width: "100%",
+                                height: "auto",
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={8} md={8}>
+                        <Grid container spacing={1}>
+                            <Grid item xs={12}>
+                                <Typography
+                                    variant="h2"
+                                    sx={{ fontWeight: "400" }}
+                                >
+                                    {league.league.name}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={2} sm={1}>
+                                <img
+                                    src={league.country.flag}
+                                    alt={league.country.name}
+                                    width="150"
+                                    height="150"
+                                    style={{
+                                        width: "100%",
+                                        height: "auto",
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={10} sm={11}>
+                                <Typography variant="h6">
+                                    {league.country.name}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} md={2}>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <Stack spacing={3}>
+                                <DatePicker
+                                    views={["year"]}
+                                    label="Select Season"
+                                    value={season}
+                                    onChange={(newSeason) => {
+                                        let newYear = new Date(
+                                            newSeason
+                                        ).getFullYear();
+                                        setSeason(newYear.toString());
+                                    }}
+                                    minDate={new Date("2010-01-01")}
+                                    maxDate={new Date(`${season}-01-01`)}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            helperText={null}
+                                        />
+                                    )}
+                                />
+                            </Stack>
+                        </LocalizationProvider>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
+}
