de1c1acd0b40828d4168b2f0591740b66900cab9
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-19 15:21:07 +0000

    refactor: change LoadingSkeleton to new search display

--- src/components/skeletons/LoadingSkeleton.js
+++ src/components/skeletons/LoadingSkeleton.js
@@ -1,35 +1,30 @@
 import React from "react";
-import { Skeleton, Grid, Typography, Stack } from "@mui/material";
+import { Skeleton, Grid, Typography, Card, CardContent } from "@mui/material";
 
 export default function LoadingSkeleton() {
-    const itemSkeleton = () => {
-        return (
-            <li style={{ listStyle: "none" }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={2}>
-                        <Skeleton
-                            variant="rectangular"
-                            width="100%"
-                            height="100%"
-                        />
+    return [...Array(5)].map((_, i) => (
+        <Grid item xs={12} key={i}>
+            <Card>
+                <CardContent>
+                    <Grid container spacing={2}>
+                        <Grid item xs={2}>
+                            <Skeleton
+                                variant="rectangular"
+                                width="100%"
+                                height="100%"
+                            />
+                        </Grid>
+                        <Grid item xs={10}>
+                            <Typography component="div" width="80%">
+                                <Skeleton />
+                            </Typography>
+                            <Typography component="div" width="20%">
+                                <Skeleton />
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={10}>
-                        <Typography>
-                            <Skeleton />
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </li>
-        );
-    };
-
-    return (
-        <Stack direction="column" spacing={2}>
-            {itemSkeleton()}
-            {itemSkeleton()}
-            {itemSkeleton()}
-            {itemSkeleton()}
-            {itemSkeleton()}
-        </Stack>
-    );
+                </CardContent>
+            </Card>
+        </Grid>
+    ));
 }
